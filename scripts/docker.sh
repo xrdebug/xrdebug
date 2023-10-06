eb6d56793f0c41eb00ca5cd5b5c804eb32ace1f4
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2023-10-05 21:26:35 -0300

    garbage

--- scripts/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker run --pull=always --rm --init -i -p 27420:27420 ghcr.io/chevere/xrdebug:latest
--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-CURRENT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-PARENT_DIRECTORY="${CURRENT_DIRECTORY%/*}"
-${PARENT_DIRECTORY}/xr -p 27420
